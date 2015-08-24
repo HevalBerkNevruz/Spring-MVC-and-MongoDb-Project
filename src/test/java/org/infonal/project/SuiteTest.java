ecbc3f4addcfae6504ddada7155439eac8d33bbe
Heval Berk <hevalberknevruz@gmail.com>
2015-08-24 23:54:41 +0300

    Add  Test Suite

---
+++ src/test/java/org/infonal/project/SuiteTest.java
@@ -0,0 +1,15 @@
+package org.infonal.project;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by heval on 24.08.2015.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        UserControllerTest.class,
+        UserServiceTest.class,
+})
+public class SuiteTest {
+}
